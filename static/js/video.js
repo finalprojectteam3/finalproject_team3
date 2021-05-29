af7324e1dbcd15e1856b39a67cb9df37e42006bd
unknown <galciluz@gmail.com>
2021-05-28 20:04:59 -0400

    adding capture code

--- static/js/video.js
+++ static/js/video.js
@@ -2,13 +2,14 @@ let canvas =document.querySelector("#canvas")
 let context=canvas.getContext("2d");
 let video=document.querySelector("#videoElement");
 
-if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+
+ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({ video: true }).then((stream) => {
         video.srcObject = stream;
         //play video
         video.play();
     });
-}
+ }
 
 document.getElementById("snap").addEventListener("click", () =>{
     context.drawImage(video,0,0,640,480);
